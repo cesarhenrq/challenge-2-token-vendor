7d230f00c66579d1e39d3f650fb5fd3786d5330c
Cesar Henrique <cesaarhvb@gmail.com>
2025-05-10 18:14:50 -0300

    Add sellTokens function and SellTokens event to Vendor contract

--- packages/hardhat/contracts/Vendor.sol
+++ packages/hardhat/contracts/Vendor.sol
@@ -8,6 +8,7 @@ contract Vendor is Ownable {
     uint256 public constant tokensPerEth = 100;
 
     event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+    event SellTokens(address seller, uint256  amountOfTokens, uint256 amountOfETH);
 
     YourToken public yourToken;
 
@@ -21,10 +22,13 @@ contract Vendor is Ownable {
         emit BuyTokens(msg.sender, msg.value, tokens);
     }
 
-    // ToDo: create a withdraw() function that lets the owner withdraw ETH
     function withdraw() public onlyOwner {
         payable(owner()).transfer(address(this).balance);
     }
 
-    // ToDo: create a sellTokens(uint256 _amount) function:
+    function sellTokens(uint256 _amount) public {
+        yourToken.transferFrom(msg.sender, address(this), _amount);
+        payable(msg.sender).transfer(_amount / tokensPerEth);
+        emit SellTokens(msg.sender, _amount, _amount / tokensPerEth);
+    }
 }
